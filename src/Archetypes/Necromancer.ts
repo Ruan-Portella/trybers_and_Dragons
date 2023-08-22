ceade3e84a75a5f522ee2ef6e98b9c92f01b5f21
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-22 16:27:43 -0300

    Feat: Cria classe Necromancer

---
+++ src/Archetypes/Necromancer.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Necromancer extends Archetype {
+  private readonly _damage: EnergyType;
+  private static instances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._damage = 'mana';
+  }
+
+  get energyType(): EnergyType {
+    return this._damage;
+  }
+
+  static createdArchetypeInstances(): number {
+    Necromancer.instances += 1;
+    return Necromancer.instances;
+  }
+}
