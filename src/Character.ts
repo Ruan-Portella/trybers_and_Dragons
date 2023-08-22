246730b28f9c565d9f94d1fbe9902c2d457411de
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-22 17:49:35 -0300

    Feat: Refatora attack e special para atacar um SimpleFighter

--- src/Character.ts
+++ src/Character.ts
@@ -1,6 +1,6 @@
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
@@ -79,7 +79,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  public attack(enemy: Fighter): void {
+  public attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this.strength);
   }
 
@@ -98,7 +98,7 @@ export default class Character implements Fighter {
     }
   }
 
-  public special(enemy: Fighter): void {
+  public special(enemy: Fighter | SimpleFighter): void {
     const dice = getRandomInt(1, 10);
     const damage = this.strength * dice;
     enemy.receiveDamage(damage);
