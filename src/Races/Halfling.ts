c531458d9a6b452b3ce7f14e4eb55c283f2b6b95
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-22 16:05:14 -0300

    Feat: Cria raça Halfling

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+    public static instances: number = 0;
+    private _maxLifePoints: number 
+
+    constructor(name: string, dexterity: number) {
+        super(name, dexterity)
+        this._maxLifePoints = 60;
+    }
+
+    public get maxLifePoints(): number {
+        return this._maxLifePoints;
+    }
+
+    public static createdRacesInstances(): number {
+        Halfling.instances += 1;
+        return Halfling.instances;
+    }
+}
