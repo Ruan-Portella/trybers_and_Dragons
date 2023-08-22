f6cda01eb4af4d6d8b000ba6500921bf1ce6d0fb
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-22 16:28:07 -0300

    Feat: importa e exporta classes

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,14 @@
 import Archetype from './Archetype';
+import Mage from './Mage';
+import Warrior from './Warrior';
+import Necromancer from './Necromancer';
+import Ranger from './Ranger';
 
 export default Archetype;
+
+export {
+  Mage, 
+  Warrior,
+  Necromancer,
+  Ranger,
+};
