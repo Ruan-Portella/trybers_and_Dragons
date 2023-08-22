f7972624b53bf0fdb43d5b0a771955b0931dd2a4
Ruan-Portella <ruanmorales29@gmail.com>
2023-08-22 16:05:06 -0300

    Feat: Cria raça Elf

---
+++ src/Races/Elf.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+    public static instances: number = 0;
+    private _maxLifePoints: number 
+
+    constructor(name: string, dexterity: number) {
+        super(name, dexterity)
+        this._maxLifePoints = 99;
+    }
+
+    public get maxLifePoints(): number {
+        return this._maxLifePoints;
+    }
+
+    public static createdRacesInstances(): number {
+        Elf.instances += 1;
+        return Elf.instances;
+    }
+}
